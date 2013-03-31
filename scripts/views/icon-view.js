8826bd782f7184bf54933f688158134b56f8b955
Bear Travis <betravis@adobe.com>
2013-03-30 19:30:15 -0700

    edit labels

--- scripts/views/icon-view.js
+++ scripts/views/icon-view.js
@@ -1,9 +1,14 @@
 icomatic.Views.IconView = Backbone.View.extend({
+events: {
+    'blur .icon-label': 'blurHandler',
+    'focus .icon-label': 'focusHandler',
+    'keydown .icon-label': 'keydownHandler'
+},
 template: _.template(
 '<div class=\'icon\'>\
   <%= svg %>\
 </div>\
-<div class=\'icon-label\'><%= name %></div>'
+<div class=\'icon-label\' contenteditable><%= name %></div>'
 ),
 tagName: 'li',
 className: '',
@@ -11,5 +16,20 @@ render: function() {
     var result = this.template({ svg: this.model.get('svg'), name: this.model.get('name') });
     this.el.innerHTML = result;
     return this;
+},
+blurHandler: function(event) {
+    var label = this.el.querySelector('.icon-label');
+    var content = label.innerHTML;
+    if (content === label.getAttribute('data-value'))
+        return;
+    this.model.set('name', content);
+},
+focusHandler: function(event) {
+    var label = this.el.querySelector('.icon-label');
+    label.setAttribute('data-value', this.model.get('name'));
+},
+keydownHandler: function(event) {
+    if (event.keyCode == 13)
+        this.el.querySelector('.icon-label').blur();
 }
 });
